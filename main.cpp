5fbd6496ec55bb38386bcbebfdb54d14b177e013
joeyrag <85812622+joeyrag@users.noreply.github.com>
2021-07-17 20:32:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,23 @@
 using namespace std;
 
 int main(){
-	int choice;// This might be changed into a string
+	int choice = 1;// This might be changed into a string
 	int MFunc();
 	EmployeeClass staff ;
 
 	cout<<"Start program"<<endl;
 	MFunc();
-	staff.print();
+	cout<<"Choose an option"<<endl;
+
+	do{
+		cout<<"1. Employee options"<<endl;
+		cout<<"2. Movie options"<<endl;
+		cout<<"3. Room options"<<endl;
+		cout<<"3. Tickets option"<<endl;
+		cin>>choice;
+	if(choice == 1){staff.print();}
+
+	} while(choice == 1);
 
 	cout<<"End program"<<endl;
 }
